9aa3c631a2548b698ad271dcc56f6e00af2ad1b3
underwindfall <14819756+underwindfall@users.noreply.github.com>
2020-04-09 23:21:35 +0200

    fix display issues

--- src/middleware/profile-middleware.js
+++ src/middleware/profile-middleware.js
@@ -1,22 +1,18 @@
-const Profile = require("../profiles/profile")
-const ProfileManager = require("../profiles/profile-manager")
-const Logger = require("../utils/logger")
-const Configuration = require("../configuration")
+const Profile = require('../profiles/profile');
+const ProfileManager = require('../profiles/profile-manager');
+const Logger = require('../utils/logger');
+const Configuration = require('../configuration');
 
 function loadProfile(req, res, next) {
-    const routeRegex = Configuration.load('prefix').routeRegex
-    const regExp = new RegExp(routeRegex + "(.+)\/.+", 'i')
-    const results = regExp.exec(req.url)
-    console.log("loadProfile","regExp"+routeRegex +"admin/profiles","======req.url"+req.url)
-    var profileName = undefined
-	if ( results != undefined ) {
-		profileName = results[1]
+    const routeRegex = Configuration.load('prefix').routeRegex;
+    const regExp = new RegExp(routeRegex + '(.+)/.+', 'i');
+    const results = regExp.exec(req.url);
+    var profileName = undefined;
+    if (results != undefined) {
+        profileName = results[1];
     }
-
-    console.log("profileName",profileName)
-    console.log("profileName",ProfileManager.default.profileNamed(profileName) )
-    req.profile = ProfileManager.default.profileNamed(profileName) || Profile.default
-	next()
+    req.profile = ProfileManager.default.profileNamed(profileName) || Profile.default;
+    next();
 }
 
-module.exports = loadProfile
+module.exports = loadProfile;
