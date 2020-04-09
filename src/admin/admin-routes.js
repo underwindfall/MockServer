0d979bf978fb5d825fa40862c44fac8ed6e49469
Qifan YANG <qifan.yang@netatmo.com>
2020-04-09 23:21:35 +0200

    fix display issues

--- src/admin/admin-routes.js
+++ src/admin/admin-routes.js
@@ -1,43 +1,45 @@
 const ProfileManager = require('../profiles/profile-manager').default;
 const RouteManager = require('../routes/route-manager').default;
 
+const pathPrefix = '/mock/security';
 class AdminRoutes {
     /// Build the admin scetions routes.
     ///
     /// - parameter app: The Express app.
     static build(app) {
-        app.get('/admin', (req, res) => {
-            res.redirect('/admin/profiles');
+        app.get(`${pathPrefix}/admin`, (req, res) => {
+            res.redirect(`${pathPrefix}/admin/profiles`);
         });
 
-        app.get('/admin/profiles', (req, res) => {
-            res.render('index', {
-                profiles: ProfileManager.listProfiles(),
-            });
-        });
-
-        app.get('/admin/profiles/:profile', (req, res) => {
-            AdminRoutes.renderProfile(req, res);
-        });
-
-        app.post('/admin/profiles/:profile', (req, res) => {
-            req.params.updated = ProfileManager.updateProfile(req.params.profile, req.body);
-            AdminRoutes.renderProfile(req, res);
-        });
+        app.get(`${pathPrefix}/admin/profiles`, (req, res) => {
+			res.render('index', {
+                pathPrefix: pathPrefix,
+                profiles: ProfileManager.listProfiles()
+			})
+        })
+        
+    	app.get(`${pathPrefix}/admin/profiles/:profile`, (req, res) => {
+			AdminRoutes.renderProfile(req, res)
+		})
+		
+		app.post(`${pathPrefix}/admin/profiles/:profile`, (req, res) => {
+			req.params.updated = ProfileManager.updateProfile(req.params.profile, req.body)
+			AdminRoutes.renderProfile(req, res)
+		})
     }
 
     static renderProfile(req, res) {
         const profileName = req.params.profile || 'default';
-        if (!profileManager.profileExists(profileName)) {
-            res.redirect('/admin/profiles');
+        if (!ProfileManager.profileExists(profileName)) {
+            res.redirect(`${pathPrefix}/admin/profiles`)
             return;
         }
-
-        let profile = ProfileManager.profileNamed(profileName);
-        let routes = RouteManager.list();
+        const profile = ProfileManager.profileNamed(profileName);
+        const routes = RouteManager.list();
         res.render('profile', {
             profile: profile,
             routes: routes,
+            pathPrefix: pathPrefix,
             updated: req.params.updated != undefined,
             success: !!req.params.updated,
         });
